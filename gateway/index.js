02955c4518f1878eed9864861530ad759b216174
Shakeel Khan <shinwari.khan474@chainz.co>
2023-01-11 12:50:51 +0500

    working with microservices for online shopping backend using node.js

---
+++ gateway/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const proxy = require('express-http-proxy');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/customer', proxy('http://localhost:9001'));
+app.use('/shopping', proxy('http://localhost:9003'));
+app.use('/', proxy('http://localhost:9002')); // 
+
+
+
+app.listen(9000, () => {
+    console.log('Product is listening at PORT 9000...');
+});
